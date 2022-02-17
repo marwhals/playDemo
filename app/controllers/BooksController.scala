f82f84e13aae06eaa79bb9db25aaba47db93de5a
marwhals <marjan.mubarok2012@gmail.com>
2022-02-17 20:37:59 +0000

    Added method names

---
+++ app/controllers/BooksController.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.mvc.Controller
+
+class BooksController extends Controller {
+
+  def index = ???
+  def create = ???
+  def save = ???
+  def edit(id : Int) = ???
+  def update = ???
+  def destroy(id : Int) = ???
+  def show(id: Int) = ???
+
+
+
+}
