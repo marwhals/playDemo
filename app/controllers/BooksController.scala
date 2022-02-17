dce79fcbfd06531ecee432f09f2d6ace3e629cde
marwhals <marjan.mubarok2012@gmail.com>
2022-02-17 20:52:33 +0000

    Added routes

--- app/controllers/BooksController.scala
+++ app/controllers/BooksController.scala
@@ -1,16 +1,33 @@
 package controllers
 
+import javax.inject.Inject
+import play.api.mvc.Results.Ok
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents, Request}
 import play.mvc.Controller
 
-class BooksController extends Controller {
+class BooksController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  def index = ???
-  def create = ???
-  def save = ???
-  def edit(id : Int) = ???
-  def update = ???
-  def destroy(id : Int) = ???
-  def show(id: Int) = ???
+  def index = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+  def create = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+  def save = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+  def edit(id : Int) = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+  def update = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+  def destroy(id : Int) = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+  def show(id: Int) = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
 
 
 
